aedd40c110c881a89471d8404647ed3f391b6561
Adam Farley <adamfrly@gmail.com>
2023-07-04 19:49:46 -0400

    day 1

--- python/solutions/day01.py
+++ python/solutions/day01.py
@@ -1,8 +1,11 @@
+import time
 from python.utils import get_input
 
-
 def main(input):
-    weights = 2
+    weights = list(map(sum, [list(map(int, x.split("\n"))) for x in input.rstrip("\n").split("\n\n")]))
+    print(max(weights))
 
 if __name__ == '__main__':
+    start = time.perf_counter()
     main(get_input(1))
+    print(f"{(time.perf_counter() - start) * 1000:.3f}ms")
