4f9ae1575c462a0d28872f829d6bb49ec5d90d63
Adam Farley <adamfrly@gmail.com>
2023-07-04 20:52:07 -0400

    Clean up day 3

--- python/solutions/day03.py
+++ python/solutions/day03.py
@@ -7,13 +7,14 @@ def main(input):
     sacks = input.split("\n")
     result = 0
     for sack in sacks:
-        half_1 = list(sack[:len(sack)//2])
-        half_2 = list(sack[len(sack)//2:])
-        priority = ord(set(half_1).intersection(half_2).pop())
-        priority = priority - 96 if priority >= 97 else priority - 38
-        result += priority
+        half_1, half_2 = list(sack[:len(sack)//2]), list(sack[len(sack)//2:])
+        overlap = set(half_1).intersection(half_2).pop()
+        result += get_priority(overlap)
     print(result)
-    
+
+def get_priority(ch):
+    return 1 + (ord(ch) - ord('a') if ch.islower() else ord(ch) - ord("A") + 26)
+
 if __name__ == '__main__':
     start = time.perf_counter()
     main(get_input(day))
