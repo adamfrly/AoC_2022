35b5154a41c03999460878b6875abaa1c1183de5
Adam Farley <adamfrly@gmail.com>
2023-07-04 20:12:18 -0400

    day 2

---
+++ python/solutions/day02.py
@@ -0,0 +1,23 @@
+import time
+from python.utils import get_input
+
+RESULTS = {"Z": "B", "X": "C", "Y": "A"}
+
+def main(input):
+    points = 0
+    games = [game.split(" ") for game in input.split("\n")]
+    for game in games:
+        game_points = ord(game[1]) - 87
+        if ord(game[0]) + 23 == ord(game[1]):
+            game_points += 3
+        elif RESULTS[game[1]] == game[0]:
+            game_points += 6
+        print(game_points)
+        points += game_points
+    print(points)
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    main(get_input(2))
+    print(f"{(time.perf_counter() - start) * 1000:.3f}ms")
