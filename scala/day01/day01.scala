45d5be1970640c25a1c96a01f42ae7051c4d5f9d
adamfrly <adamfrly@gmail.com>
2023-08-01 23:06:41 -0400

    Add scala

---
+++ scala/day01/day01.scala
@@ -0,0 +1,8 @@
+import scala.io
+
+object day01 {
+    def main(args: Array[String]) = {
+        val input = scala.io.Source.fromFile("../../inputs/day01.txt").mkString
+        println(input)
+    }
+}
---
+++ scala/template.scala
@@ -0,0 +1,8 @@
+import scala.io
+
+object day0X {
+    def main(args: Array[String]) = {
+        val input = scala.io.Source.fromFile("../inputs/day0X.txt").mkString
+        println(input)
+    }
+}
