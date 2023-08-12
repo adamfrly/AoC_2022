f8dd8cebeefef997aea320777dafaa27f46b14e2
Adam Farley <adamfrly@gmail.com>
2023-08-11 23:12:56 -0400

    Add Day 1 scala

--- scala/day01/day01.scala
+++ scala/day01/day01.scala
@@ -2,7 +2,27 @@ import scala.io
 
 object day01 {
     def main(args: Array[String]) = {
-        val input = scala.io.Source.fromFile("../../inputs/day01.txt").mkString
-        println(input)
+        val input = io.Source.fromFile("../../inputs/day01.txt").getLines().toSeq
+        val counts = input.map(customToInt(_)) 
+        var calorie_count: Int = 0
+        var calorie_counts: Seq[Int] = Seq()
+        for (meal <- counts) {
+            if (meal > 0) {
+                calorie_count += meal
+            }
+            else {
+                calorie_counts = calorie_counts :+ calorie_count
+                calorie_count = 0
+            }
+        }
+        println(calorie_counts.max)
+    }
+
+    def customToInt(s: String): Int = {
+        try {
+            Integer.parseInt(s.trim)
+        } catch {
+            case e: Exception => 0
+        }
     }
 }
