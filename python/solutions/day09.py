d5f2976307691c35cd4a8bf22eb376d3c5b01b8c
adamfrly <adamfrly@gmail.com>
2024-07-02 14:11:05 -0400

    Unfinished day 9

---
+++ python/solutions/day09.py
@@ -0,0 +1,12 @@
+import time
+from python.utils import get_input
+
+day = 9
+
+def main(input):
+    print(input)
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    main(get_input(day))
+    print(f"{(time.perf_counter() - start) * 1000:.3f}ms")
